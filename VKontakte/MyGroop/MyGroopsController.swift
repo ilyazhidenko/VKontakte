4bb8ab3586ec58f78fa9b32766273a0a282e62f9
Ilya Zhidenko <ily20692005@icloud.com>
2020-02-08 21:37:34 +0300

    ошибка при попытке добавления в список

--- VKontakte/MyGroop/MyGroopsController.swift
+++ VKontakte/MyGroop/MyGroopsController.swift
@@ -10,41 +10,15 @@ import UIKit
 
 class MyGroopsController: UITableViewController {
     @IBAction func addGroop(segue: UIStoryboardSegue){
-        if segue.identifier == "addGroup"{
-            let allGroupController = segue.source as! AllGroopController
-            if let indexPath = allGroupController.tableView.indexPathForSelectedRow{
-                let groop = allGroupController.groop[indexPath.row]
-            
+        if segue.identifier == "addGroop" {
+            let AllGroopController = segue.source as! AllGroopController
+            if let indexPath = AllGroopController.tableView.indexPathForSelectedRow {
+               let groop = AllGroopController.groop[indexPath.row]
+                groop.append(groop)
                 tableView.reloadData()
             }
         }
     }
-    
- /*   var mygroops = [String]()
-
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        
-    }
-
-    override func numberOfSections(in tableView: UITableView) -> Int {
-        // #warning Incomplete implementation, return the number of sections
-        return 1
-    }
-
-    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        // #warning Incomplete implementation, return the number of rows
-        return mygroops.count
-    }
-
-    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let cell = tableView.dequeueReusableCell(withIdentifier: "MyGroopCell", for: indexPath) as! MyGroopsCell
-        let mygroop = mygroops[indexPath.row]
-        cell.MyGroop.text = mygroops
-        return cell
-    }
-
-}*/
 
     var mygroop = [String]()
 
